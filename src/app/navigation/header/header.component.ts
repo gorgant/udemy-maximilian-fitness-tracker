35f4a9caeccabccb31461ff05435d18290852a15
Gorgant CX1 <gorgant@gorgant.com>
2018-12-17 15:26:34 +0800

    refactor sidenav into separate components

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() toggleSidenav = new EventEmitter<void>();
+  @ViewChild('matButton') matButton;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onToggleSidenav() {
+    this.toggleSidenav.emit();
+
+    // This hacky solution is required to remove ripple effect from menu icon after closing sidenav
+    this.matButton._elementRef.nativeElement.blur();
   }
 
 }
